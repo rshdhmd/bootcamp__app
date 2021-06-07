12e44fdb579ee5e4fd0d9d62470ac9e9f7d5abdd
Rashad Hameed <81767683+rshdhmd@users.noreply.github.com>
2021-06-07 18:08:53 +0530

    day 3 files adde

--- lib/photos_model.dart
+++ lib/photos_model.dart
@@ -1,9 +1,7 @@
 class PhotosModel {
   final String id;
   final String imgURL;
+  final String? name;
 
-  PhotosModel({
-    required this.id,
-    required this.imgURL,
-  });
+  PhotosModel({required this.id, required this.imgURL, this.name});
 }
